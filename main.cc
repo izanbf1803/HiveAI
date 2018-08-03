316265ec920d38dc62fe4258e4db4c7ae9f24b2c
Izan Beltrán <izanbf1803@gmail.com>
2018-08-03 14:04:23 +0200

    Implementing Hex grid drawing

--- main.cc
+++ main.cc
@@ -33,6 +33,39 @@ using namespace Hive;
 using namespace std;
 
 const int WIDTH = 800, HEIGHT = 600;
+const int offset_x = 16, offset_y = 14;
+
+SDL_Renderer* renderer = NULL;
+SDL_Surface* hexgrid_img = NULL;
+SDL_Texture* hexgrid_tex = NULL;
+int hex_w = -1, hex_h = -1;
+
+void draw_circle(int cx, int cy, int r)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 1);
+    int r2 = r*r;
+    for (int x = -r; x <= r; ++x) {
+        for (int y = -r; y <= r; ++y) {
+            if (x*x + y*y <= r2) {
+                SDL_RenderDrawPoint(renderer, cx+x, cy+y);
+            }
+        }
+    }
+}
+
+void draw_hexgrid()
+{
+    assert(hex_w >= 0 and hex_h >= 0);
+
+    for (int i = -1; i * hex_w <= WIDTH; ++i) {
+        for (int j = -1; j * hex_h <= HEIGHT; ++j) {
+            int x = i * hex_w;
+            int y = j * hex_h - (i&1 ? hexgrid_img->h/2 : 0);
+            SDL_Rect dstrect = { x, y, hexgrid_img->w, hexgrid_img->h };
+            SDL_RenderCopy(renderer, hexgrid_tex, NULL, &dstrect);
+        }
+    }
+}
 
 int main( int argc, char *argv[] )
 {
@@ -47,7 +80,6 @@ int main( int argc, char *argv[] )
         return 1;
     }
     
-    SDL_Event windowEvent;
     Game game(Piece::Ant);
     auto g = game.get_grid();
     for (int y = 0; y < GSIDE; ++y) {
@@ -62,15 +94,38 @@ int main( int argc, char *argv[] )
     for (Hex h : game.valid_spawns((Color)1)) {
     	cout << "::" << h << endl;
     }
-    
+
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    hexgrid_img = SDL_LoadBMP("img/hexagon.bmp");
+    hexgrid_tex = SDL_CreateTextureFromSurface(renderer, hexgrid_img);
+    hex_w = 24;
+    hex_h = hexgrid_img->h;
+
+    SDL_Event windowEvent;
     while (true) {
         if (SDL_PollEvent(&windowEvent)) {
             if (SDL_QUIT == windowEvent.type) {
                 break;
             }
         }
-    }
-    
+        
+        draw_hexgrid();
+        for (int i = 0; i * hex_w < WIDTH; ++i) {
+            for (int j = 0; j * hex_h < HEIGHT; ++j) {
+                int x = i * hex_w;
+                int y = j * hex_h - (i&1 ? hexgrid_img->h/2 : 0);
+                draw_circle(x+offset_x, y+offset_y, 10);
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_RenderClear(renderer);
+    }   
+
+    SDL_DestroyTexture(hexgrid_tex);
+    SDL_FreeSurface(hexgrid_img);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     
