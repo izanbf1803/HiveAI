091c94f9b0464c0ad756cabcce70cf65769b4ba4
Izan Beltrán <izanbf1803@gmail.com>
2019-01-10 12:25:10 +0100

    Backup, added base code to implement Monte Carlo

--- AI.h
+++ AI.h
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <ctime>
 
+#define USE_HEURISTIC 1
+
 namespace AI
 {
 	typedef long long ll; // easier to type
@@ -140,90 +142,82 @@ namespace AI
 			return best_play;
 		}
 
+		V<PlayInfo> plays;
+		plays.reserve(256);
+
 		V<Hex> vspawns = game.valid_spawns(color);
-		random_shuffle(vspawns.begin(), vspawns.end());
 		for (Piece piece : PIECES) {
 			for (Hex p : vspawns) {
-				if (game.put_piece(p.x, p.y, color, piece, true)) {
-					auto lambda_restore = [&](){
-						game.destroy(Hex(0, p.x, p.y));
-					};
-					PlayInfo play = play_info_put(0, p, piece);
-					
-					if (depth == MAXDEPTH or (depth == MAXDEPTH-1 and delta_time() > 4000)) {
-						play.score = get_heuristic_score(game);
-					}
-					else {
-						play.score = minimax(game, (Color)!color, depth+1, alpha, beta).score;
-					}
-					// D(play.score) << endl;
-
-					// !!! MINIMAX START !!!
-					if (color == ia_color) { // maximize
-						if (play > best_play) best_play = play;
-						alpha = max(alpha, best_play.score);
-					}
-					else { // minimize
-						if (play < best_play) best_play = play;
-						beta = min(beta, best_play.score);
-					}
-
-					if (beta <= alpha) {
-						lambda_restore();
-						return best_play;
-					}
-					// !!! MINIMAX END !!!
-
-					lambda_restore();
-				}
+				plays.push_back(play_info_put(0, p, piece));
 			}
 		}
 
-		for (int layer = 0; layer < 2; ++layer) {
-			for (int y = 0; y < GSIDE; ++y) {
-				for (int x = 0; x < GSIDE; ++x) {
-					Hex h = game.grid[x][y][layer];
-					if (h.color == color) {
-						V<Hex> vmoves = game.valid_moves(h);
-						random_shuffle(vmoves.begin(), vmoves.end());
-						// D(h), D(h.piece), D(vmoves.size()) << endl;
-						for (Hex p : vmoves) {
-							if (game.move_piece(p.x, p.y, h, p.layer, true)) {
-								auto lambda_restore = [&]() {
-									game.destroy(p); // always destroy first to avoid piece tracking errors
-									game.spawn(h.x, h.y, h.color, h.piece, h.layer);
-								};
-								PlayInfo play = play_info_move(0, h, p);
+		for (Piece piece : PIECES) {
+			for (Hex h : game.positions[color][piece]) {
+				for (Hex p : game.valid_moves(h)) {
+					plays.push_back(play_info_move(0, h, p));
+				}	
+			}
+		}
+		// for (int layer = 0; layer < 2; ++layer) {
+		// 	for (int y = 0; y < GSIDE; ++y) {
+		// 		for (int x = 0; x < GSIDE; ++x) {
+		// 			Hex h = game.grid[x][y][layer];
+		// 			if (h.color == color) {
+		// 				for (Hex p : game.valid_moves(h)) {
+		// 					plays.push_back(play_info_move(0, h, p));
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
 
-								if (depth == MAXDEPTH or (depth == MAXDEPTH-1 and delta_time() > 4000)) {
-									play.score = get_heuristic_score(game);
-								}
-								else {
-									play.score = minimax(game, (Color)!color, depth+1, alpha, beta).score;
-								}
-								// D(play.score) << endl;
+		random_shuffle(plays.begin(), plays.end());
 
-								// !!! MINIMAX START !!!
-								if (color == ia_color) { // maximize
-									if (play > best_play) best_play = play;
-									alpha = max(alpha, best_play.score);
-								}
-								else { // minimize
-									if (play < best_play) best_play = play;
-									beta = min(beta, best_play.score);
-								}
+		while (not plays.empty()) {
+			PlayInfo play = plays.back();
+			plays.pop_back();
 
-								if (beta <= alpha) {
-									lambda_restore();
-									return best_play;
-								}
-								// !!! MINIMAX END !!!
+			bool valid;
+			if (play.type == PlayType::Put) {
+				valid = game.put_piece(play.h.x, play.h.y, color, play.piece, true);
+			}
+			else {
+				valid = game.move_piece(play.h2.x, play.h2.y, play.h, play.h2.layer, true);
+			}
 
-								lambda_restore();
-							}
-						}
-					}
+			if (valid) {
+				if (depth == MAXDEPTH or (depth == MAXDEPTH-1 and delta_time() > 4000)) {
+#if USE_HEURISTIC
+					play.score = get_heuristic_score(game);
+#else // use montecarlo:
+					play.score = monte_carlo_score(game);
+#endif
 				}
+				else {
+					play.score = minimax(game, (Color)!color, depth+1, alpha, beta).score;
+				}
+				// D(play.score) << endl;
+
+				if (color == ia_color) { // maximize
+					if (play > best_play) best_play = play;
+					alpha = max(alpha, best_play.score);
+				}
+				else { // minimize
+					if (play < best_play) best_play = play;
+					beta = min(beta, best_play.score);
+				}
+				
+				// Restore:
+				if (play.type == PlayType::Put) { 
+					game.destroy(Hex(0, play.h.x, play.h.y));
+				}
+				else {
+					game.destroy(play.h2); // always destroy first to avoid piece tracking errors
+					game.spawn(play.h.x, play.h.y, play.h.color, play.h.piece, play.h.layer);
+				}
+
+				if (beta <= alpha) return best_play;
 			}
 		}
 
